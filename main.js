cc1920c59050c34ffa79dbb1aa221226d69406f9
Burke Atkerson <burkeatkerson@gmail.com>
2023-05-31 13:17:55 -0600

    Temperature converts.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//declarations
+var greetingText = "Good morning!";
+const weatherCondition = "sunny";
+const userLocation = "Texas";
+const temp = 75;
+
 //Nav Menu, open -> close
 document.querySelector("#open-nav-menu").addEventListener("click", function () {
 	document.querySelector("header nav .wrapper").classList.add("nav-open");
@@ -8,18 +14,34 @@ document
 		document.querySelector("header nav .wrapper").classList.remove("nav-open");
 	});
 
-//Greeting Section
-const greetingText = "Good morning!";
-const weatherCondition = "sunny";
-const userLocation = "Texas";
-var temp = "75.656565";
-temp = Math.round(temp);
-var tempScale = "C";
-let weatherText = `It's currently ${weatherCondition} in ${userLocation} and it's ${temp}°${tempScale} outside.`;
-
-document.querySelector("H1#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
-
 //temp conversion
+
+function tempToCel(temp) {
+	var celsius = Math.round((temp - 32) * (5 / 9));
+	return celsius;
+}
+
+//Greeting Section
+document.querySelector("H1#greeting").innerHTML = greetingText;
+document.querySelector(
+	"p#weather"
+).innerHTML = `It's currently ${weatherCondition} in ${userLocation} and it's ${temp}°F outside.`;
+
+document
+	.querySelector(".weather-group")
+	.addEventListener("click", function (e) {
+		var tempID = e.target.id;
+		if (tempID == "celsius") {
+			celsius = tempToCel(temp);
+			document.querySelector(
+				"p#weather"
+			).innerHTML = `It's currently ${weatherCondition} in ${userLocation} and it's ${celsius}°C outside.`;
+		} else if (tempID == "fahr") {
+			var fahr = temp;
+			document.querySelector(
+				"p#weather"
+			).innerHTML = `It's currently ${weatherCondition} in ${userLocation} and it's ${fahr}°F outside.`;
+		}
+	});
 
 //image gallery
