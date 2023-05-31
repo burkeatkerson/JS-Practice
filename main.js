7dda184d8ef6e237345db4888c8c3b6c1e89de51
Burke Atkerson <burkeatkerson@gmail.com>
2023-05-31 14:34:19 -0600

    Time with padStart formatting.

--- main.js
+++ main.js
@@ -8,12 +8,18 @@ const temp = 75;
 
 setInterval(function () {
 	let localTime = new Date();
-	document.querySelector("span[data-time=hours").textContent =
-		localTime.getHours();
-	document.querySelector("span[data-time=minutes").textContent =
-		localTime.getMinutes();
-	document.querySelector("span[data-time=seconds").textContent =
-		localTime.getSeconds();
+	document.querySelector("span[data-time=hours").textContent = localTime
+		.getHours()
+		.toString()
+		.padStart(2, "0");
+	document.querySelector("span[data-time=minutes").textContent = localTime
+		.getMinutes()
+		.toString()
+		.padStart(2, "0");
+	document.querySelector("span[data-time=seconds").textContent = localTime
+		.getSeconds()
+		.toString()
+		.padStart(2, "0");
 }, 1000);
 
 //Nav Menu, open -> close
